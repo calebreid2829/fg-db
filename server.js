a10d116a826e2d70dd98ea5f3807a4a5af32d182
Debian <bitnami@ip-172-26-8-69.us-east-2.compute.internal>
2024-01-18 14:22:38 +0000

    Added changes from AWS instance

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const path = require('path');
 var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
-  user: "viewer",
-  password: "viewer_password"
+  user: "db_viewer",
+  password: ""
 });
 
 //const URLSearchParams = require('URLSearchParams');
@@ -20,7 +20,7 @@ var con = mysql.createConnection({
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("express"));// default URL for website
-
+app.use('/static', express.static('express/static'));
 app.get('/',(req,res) =>{
     res.sendFile(path.join(__dirname+'/express/index.html'));
     //con.query('SELECT fighter_id,name FROM bucklers.fighters WHERE game_id=1',function(err,result,fields){
@@ -35,8 +35,8 @@ app.get('/',(req,res) =>{
 
 app.post('/fighter_stats',(req,res)=>{
     con.query(`SELECT a.fighter_id,name,health,fwd_dash_speed,back_dash_speed,fwd_dash_distance,
-    back_dash_distance FROM bucklers.fighters a
-    LEFT JOIN bucklers.fighter_stats_sf6 b
+    back_dash_distance FROM fg_db.fighters a
+    LEFT JOIN fg_db.fighter_stats_sf6 b
     ON a.fighter_id = b.fighter_id`,
     function(err,result,fields){
         if(err) throw err;
@@ -49,7 +49,7 @@ app.post('/query',(req,res)=>{
 
     const fighter_id = req.body.fighter_id
     con.query(`SELECT move_id,fighter_id,move_name,input,startup,active,recovery,on_hit,on_block
-    FROM bucklers.moves_base 
+    FROM fg_db.moves_base 
     WHERE fighter_id = ${fighter_id}
     AND damage != '-'
     ORDER BY fighter_id,move_id ASC`
